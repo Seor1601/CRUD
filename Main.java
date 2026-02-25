d8ae1d4fa38636c8e32358737a9c2090c07c21b7
Samuel Elias Ortega Rodriguez <samuelortega1601@gmail.com>
2026-02-25 15:09:09 +0000

    agrego el menu de Crud

--- Main.java
+++ Main.java
@@ -1,1 +1,44 @@
+import java.util.Scanner;
+import Crud;
+import Usuario;
+public class main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do{
+            System.out.println(===Menu===);
+            System.out.println(1.Crear Usuario);
+            System.out.println(2.Leer Usuario);
+            System.out.println(3.Actualizar usuario);
+            System.out.println(4.Eliminar usuario);
 
+            System.out.println(5.Salir);
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+
+                        break;
+                        
+
+            
+
+                    
+                
+            
+                
+                    
+            }
+        }
+
+
+
+
+    }
+}
